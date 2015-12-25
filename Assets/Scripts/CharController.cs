04ebbe63966f4af3b293f0afdd2f143d5da37ba3
Broken <kennethMontgo@gmail.com>
2015-12-25 15:29:15 -0600

    created character controller and added horizontal control. Y velocity needs to be reworked

---
+++ Assets/Scripts/CharController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharController : MonoBehaviour {
+
+	float maxspeed = 10f;
+	
+	// fixedUpdate takes into account time, so no time.deltatime necessary
+	void FixedUpdate () {
+		float move = Input.GetAxis ("Horizontal");
+		GetComponent<Rigidbody2D>().velocity= new Vector2(move* maxspeed, 0);
+	}
+}
