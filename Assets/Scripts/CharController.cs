b955cd07471d3bfcd16db5ee14b6c554a84f3f62
Broken <kennethMontgo@gmail.com>
2015-12-25 18:33:09 -0600

    added velocity based dash to character controller

--- Assets/Scripts/CharController.cs
+++ Assets/Scripts/CharController.cs
@@ -3,11 +3,28 @@ using System.Collections;
 
 public class CharController : MonoBehaviour {
 
-	float maxspeed = 10f;
+	float maxSpeed = 10f;
+	float dashSpeed= 50f;
 	
+	// fixedUpdate takes into account time, so no time.deltatime necessary
+	void Update(){
+		if (Input.GetMouseButtonDown (0)) {
+		dash ();
+		}
+	}
 	// fixedUpdate takes into account time, so no time.deltatime necessary
 	void FixedUpdate () {
 		float move = Input.GetAxis ("Horizontal");
-		GetComponent<Rigidbody2D>().velocity= new Vector2(move* maxspeed,GetComponent<Rigidbody2D>().velocity.y  );
+		GetComponent<Rigidbody2D>().velocity= new Vector2(move* maxSpeed,GetComponent<Rigidbody2D>().velocity.y  );
 	}
+	void dash() {
+		Vector3 mouseDirection;
+		mouseDirection = Input.mousePosition;
+		mouseDirection.z = 0.0f;
+		mouseDirection = Camera.main.ScreenToWorldPoint(mouseDirection);
+		mouseDirection = (mouseDirection-transform.position).normalized;
+		GetComponent<Rigidbody2D>().velocity =new Vector2(mouseDirection.x*dashSpeed,mouseDirection.y*dashSpeed);
+
+	}
+
 }
